a917e6939c6111e1ec2c955dd7748619fa9efb28
Pawel-Kigler <73522977+Pawel-Kigler@users.noreply.github.com>
2020-10-30 14:52:01 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="" class="css">
+</head>
+<body>
+<div class="con">
+
+    <div class="a">
 <?php
 
-                $conn= new mysqli("127.0.0.1","root","","index");
+                $conn= new mysqli("mysql-pawel-kigler.alwaysdata.net","217223","pawelex11","pawel-kigler_pies");
                 
                 $result=$conn->query('SELECT * FROM pracownicy');
                     echo("<table border=1>");
@@ -14,4 +26,7 @@
                             echo("</tr>");
                         }
                     echo("</table>");
-                    ?>
+                    ?></div>
+                    </div>
+</body>
+</html>
